0858273736415da68ce8d1fab21d6d16a8ec1fda
florian <floriancohenjoy@gmail.com>
2018-09-11 08:20:26 +0000

    Add new file

---
+++ si4/florian,clement.java
@@ -0,0 +1,31 @@
+public class Padawan {
+
+	public static void main(String[] args) {
+		String prenom ;
+		
+		prenom = "clement" ;
+		
+		System.out.println("le prix du pain au chocolat est " + prixTTC);
+		
+		
+	
+		
+		
+		
+		
+		
+		import java.util.Scanner ;
+
+public class sawan{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in) ;
+		System.out.println ("Veuillez saisir un nombre : 25 ") ;
+		int a = sc.nextInt();
+		System.out.println ("Vous avez saisi :" + 50) ;
+		
+	}
+
+}
+		
+		
+		
