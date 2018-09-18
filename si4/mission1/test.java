cf3bcd1e0b5bf83e6b7227adbe77435c54b25be1
florian <floriancohenjoy@gmail.com>
2018-09-18 07:09:49 +0000

    Update test.java

---
+++ si4/mission1/test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		class Test{
+			public static void main (String[] args){
+			
+			age=20; //int
+			 i=0 ; // int
+			 i=1+1 ;//int
+			 j=i ; //int
+			 k= "coucou" ; //string
+			 h=k+k ;//string
+			 majeur=(age>18) ;//char
+			
+			 }
+			} 
+
+	}
+
+}
