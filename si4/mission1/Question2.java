cd78b9822c4fc024a8f49f475e49cee61f114451
florian <floriancohenjoy@gmail.com>
2018-10-01 14:20:16 +0000

    Update Question2.java

--- si4/mission1/Question2.java
+++ si4/mission1/Question2.java
@@ -5,12 +5,12 @@ question 2
 import java.util.Scanner;
 public class Question2 {
 		public static void main (String [] args) {
-			double prixTTC; // prixTTC est un un réél double précision
-			double prixHT;  // prixHT est un réél double précision
+			double prixTTC; // prixTTC est un un réél double
+			double prixHT;  // prixHT est un réél double
 			prixHT=7;       // j'affecte la valeur 7 au prixHT
-			double tauxTVA; // tauxTVA est un réél double précision
+			double tauxTVA; // tauxTVA est un réél double 
 			tauxTVA=5;      // j'affecte la valeur 5 au tauxTVA
-			double quantite; // quantite est un réél double precision
+			double quantite; // quantite est un réél double 
 			quantite=675;    // j'affecte la valeur 675 a quantite
  prixTTC=prixHT*tauxTVA*quantite; // je calcul le prixTTC en faisant prixHT multiplié par le tauxTVA multiplié par la quantite
  System.out.println("Le prixTTC est de:"+prixTTC); // j'affiche le résultat de prixTTC
