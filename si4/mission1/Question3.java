b7b64cc89844ef6c5f71d9a45cdea507976c02d0
florian <floriancohenjoy@gmail.com>
2018-10-01 14:57:07 +0000

    Add new file

---
+++ si4/mission1/Question3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Question3 {
+		
+	public static void main (String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("veuillez saisir la quantite achetée:" );
+		long quantite = sc.nextLong();
+		System.out.println("veuillez saisir le prixHT:" );
+		double prixHT= sc.nextDouble();
+		int tauxTVA=15;
+		double prixTTC;
