82e95a7bd25ecb4e7b11ee07042bbc8d820f403a
florian <floriancohenjoy@gmail.com>
2018-10-08 20:04:08 +0000

    Update FCJ.echiquier.py

--- MathAlgo/FCJ.echiquier.py
+++ MathAlgo/FCJ.echiquier.py
@@ -13,14 +13,16 @@ begin_fill()
 for i in range(4):
     forward(600)
     left(90)
-    
+   
 
 up()
 goto(-300,-300)
 down()
+begin_fill()
 for i in range(4):
     forward(75)
     left(90)
+end_fill()
 
 up()
 goto(-150,-300)
@@ -29,10 +31,11 @@ down()
 up()
 goto(-150,-300)
 down()
+begin_fill()
 for i in range(4):
     forward(75)
     left(90)
-
+end_fill()
 up()
 goto(-75,-300)
 down()
@@ -42,6 +45,278 @@ down()
 up()
 goto(0,-300)
 down()
+begin_fill()
 for i in range(4):
     forward(75)
     left(90)
+end_fill()
+
+up()
+goto(150,-300)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+
+up()
+goto(-300,-225)
+down()
+
+up()
+goto(-225,-225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-75,-225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(75,-225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(225,-225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-300,-150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-150,-150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(0,-150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(150,-150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-225,-75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-75,-75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(75,-75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(225,-75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-300,0)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-150,0)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(0,0)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(150,0)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-225,75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-75,75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(75,75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(225,75)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-300,150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-150,150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(0,150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(150,150)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-300,225)
+down()
+
+up()
+goto(-225,225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(-75,225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(75,225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
+
+up()
+goto(225,225)
+down()
+begin_fill()
+for i in range(4):
+    forward(75)
+    left(90)
+end_fill()
