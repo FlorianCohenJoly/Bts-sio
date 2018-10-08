778c19be3591ed9cab8284b53dc6b95df5d3ceb0
florian <floriancohenjoy@gmail.com>
2018-10-08 19:19:38 +0000

    Add new file

---
+++ MathAlgo/FCJ.echiquier.py
@@ -0,0 +1,47 @@
+# echiquier
+# FLORIAN COHEN-JOLY
+
+
+from turtle import*
+speed("fastest")
+color("black")
+
+up()
+goto(-300,-300)
+down()
+begin_fill()
+for i in range(4):
+    forward(600)
+    left(90)
+    
+
+up()
+goto(-300,-300)
+down()
+for i in range(4):
+    forward(75)
+    left(90)
+
+up()
+goto(-150,-300)
+down()
+
+up()
+goto(-150,-300)
+down()
+for i in range(4):
+    forward(75)
+    left(90)
+
+up()
+goto(-75,-300)
+down()
+
+
+
+up()
+goto(0,-300)
+down()
+for i in range(4):
+    forward(75)
+    left(90)
