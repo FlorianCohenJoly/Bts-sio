1391c683cd1360df83a0efe99ee0b1723cfae480
florian <floriancohenjoy@gmail.com>
2018-11-05 11:20:41 +0000

    Update FCJ.lafourchette.td5.py

---
+++ MathAlgo/FCJ.lafourchette.td5.py
@@ -0,0 +1,17 @@
+#  jeu de la fourchette td(
+#florian cohen-joly
+
+from random import*
+
+#choix du nombre  aléatoire
+nbr_mystere=randint(0,1000)
+n=0
+
+proposition=int(input("faites une proposition :"))
+
+#tant que
+
+while n=n+9 :
+    
+
+
