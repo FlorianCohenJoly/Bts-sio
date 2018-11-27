b140409d4796dc8f7c6db59be98ad46810547977
florian <floriancohenjoy@gmail.com>
2018-11-27 10:33:07 +0000

    Add new file

---
+++ si4/Mission5/tableau entrainement.java
@@ -0,0 +1,20 @@
+package fcj;
+
+public class tableaux {
+
+	public static void main(String[] args) {
+		int[] tab = new int[3];
+		tab[0]= 45 ; 
+		tab[1]= 56 ;
+		tab[2]= 76 ;
+		
+		
+		for(int a=0;a<=2;a++)
+		{
+			System.out.println(tab[a]);
+		}
+				
+
+	}
+
+}
