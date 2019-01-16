56d77f51abd7a551a0feed222f09c48461964fe1
florian <floriancohenjoy@gmail.com>
2019-01-16 15:03:38 +0000

    Add new file

---
+++ SI5/Exercice4.php
@@ -0,0 +1,23 @@
+<?php 
+	$a=2;
+	$a=$a-1;
+	$a++;
+	$b=8;
+	$b+=2;
+	$c=$a+$b*$b;
+	$c+4;
+	$d=$a*$b+$b;
+	$e=$a*($b+$b);
+	$e--;
+	$f=$a*$b/$a;
+	$f++;
+	$g=$b/$a*$a;
+	$g-=2;
+	echo $a;
+	echo $b;
+	echo $c;
+	echo $d;
+	echo $e;
+	echo $f;
+	echo $g;
+?>
