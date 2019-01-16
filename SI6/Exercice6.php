a61185fe87131552b6780dbf960ba43967541136
florian <floriancohenjoy@gmail.com>
2019-01-16 15:47:03 +0000

    Add new file

---
+++ SI6/Exercice6.php
@@ -0,0 +1,33 @@
+<!doctype thml>
+<html>
+
+<head>
+<metacharset="utf-8"/>
+<title>Ma page de présentation</title>
+</head>
+
+<body>
+<h1>Ma présentation</h1>
+
+<?php
+
+	$prenom = "Bill" ;
+	$classe = 'BTS SIO';
+	$nbLangages = 2 ;
+		
+?>
+
+<p>
+<?php
+
+	echo "Bonjour, je m'appelle $prenom et je suis en $classe.";
+	//Un commentaire
+	echo"</br>" ;
+	echo "Je connais $nbLangages langages de programmation";
+	
+?>
+
+</p>
+</body>
+
+</html>
