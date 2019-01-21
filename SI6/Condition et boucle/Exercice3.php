9ae81c8964328dc1c2d2495173df34f13456d4fc
florian <floriancohenjoy@gmail.com>
2019-01-21 14:48:32 +0000

    Add new file

---
+++ SI6/Condition et boucle/Exercice3.php
@@ -0,0 +1,30 @@
+<?php 
+
+$nbre=5; 
+$somme=0; 
+
+	for ($i=1;$i<=$nbre;$i++) { 
+	
+		$somme = $somme+$i ;
+		
+} 
+
+echo "La somme des entiers de 1 à 5 vaut $somme"; 
+echo"</br>";
+
+?> 
+
+<?php 
+
+$nbre=6; 
+$somme = 0;
+$i = 1;
+
+    while ($i <= 6) { 
+	    $somme = $somme + $i;
+	    $i = $i + 1;
+	}
+	
+echo "La somme des entiers de 1 à 6 vaut $somme";
+
+?>
