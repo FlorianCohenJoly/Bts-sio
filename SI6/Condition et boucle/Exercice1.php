ff9b7806e39e7da1d0e14364b805578e23c45c79
florian <floriancohenjoy@gmail.com>
2019-01-21 14:47:12 +0000

    Add new file

---
+++ SI6/Condition et boucle/Exercice1.php
@@ -0,0 +1,16 @@
+<?php
+
+$age = 25 ;
+$sexe = "feminin";
+
+if ($age>=21 && $age<=40 && $sexe == "feminin")
+	
+	{
+	echo "Bienvenue sur le site";
+	}
+	else
+	{
+		echo "Désolé vous n'avez acces";
+	}
+	
+?>
