b1e86cd38376909655e156e79f7b76400ad58a1b
florian <floriancohenjoy@gmail.com>
2019-02-04 13:51:35 +0000

    Add new file

---
+++ SI6/Tableau php/Exercice2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<head>
+	<link rel="stylesheet" href="Exercice1.css" />
+  <meta charset="utf-8">
+  
+  <title>Exercice 2</title>
+</head>
+
+<body>
+
+
+
+<table>
+
+<?php 
+
+$date = array (
+    'janvier' => '31',
+    'février' => '28',
+    'mars' => '31',
+    'avril' => '30',
+	'mai' => '31',
+    'juin' => '30',
+    'juillet' => '31',
+    'aôut' => '31',
+	'septembre' => '30',
+    'octobre' => '31',
+    'novembre' => '30',
+    'décembre' => '31');
+
+	print_r($date);
+	
+
+?>
+
+</table>
+</body>
+</html>
+
